3fdd610df81d32cb93a000d61cccd6cd2c20d8fd
ndjerrou <modernejd@gmail.com>
2020-06-08 05:18:43 +0200

    Get request to weatherstack

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("starting");
+const request = require("request");
+
+const url =
+  "http://api.weatherstack.com/current?access_key=14cf73f014272a5492845e04a1fe01ad&query=Suresnes";
+request.get(
+  {
+    url,
+  },
+  (err, { body }) => {
+    const { current } = JSON.parse(body);
+    console.log(current);
+  }
+);
